295b6cc0c7186220e86978abdbb88d36e5858ced
grey4jtb <68443426+grey4jtb@users.noreply.github.com>
2021-11-12 16:55:53 -0600

    changes to assignment1

--- assignment1.py
+++ assignment1.py
@@ -16,7 +16,7 @@ import plotly.express as px
 st.title("Assignment 1 Product Idea")
 st.markdown("-Joseph Boykin")
 st.markdown(" ")
-st.markdown("* The Application: ITEM LOCATOR ")
+st.title(" The Application: ITEM LOCATOR ")
 st.markdown(" ")
 st.markdown("● I am creating an app for stores where the customer enters the items they need, be it grocery items or items for a home repair. ")
 st.markdown("● The app will locate the items in the store and let the customer know where the items are located.")
