570cb514aeb74bb51ff6922a0e9542170d8b6a1a
grey4jtb <68443426+grey4jtb@users.noreply.github.com>
2021-12-08 11:28:39 -0600

    changes to assignment1

--- assignment1.py
+++ assignment1.py
@@ -17,7 +17,7 @@ import numpy as np
 
 st.image("https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800284/83982313-work-tools-poster-for-hardware-store-design.jpg")
 #st.image(w,h,3)
-st.image(caption="Locating items")
+st.image(caption='Locating items')
 
 
 st.title("Final Project Presentation")
