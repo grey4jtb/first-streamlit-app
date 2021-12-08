608be3ac950522b36542fa701f74170f78893250
grey4jtb <68443426+grey4jtb@users.noreply.github.com>
2021-12-08 13:02:36 -0600

    changes to assignment1

--- assignment1.py
+++ assignment1.py
@@ -8,17 +8,17 @@ import streamlit as st
 import pandas as pd
 
 import plotly.express as px
-#from spacy.lang.en.product_name import product_name
-#import pickle as pkl
-#from tqdm import tqdm
 import re
-#from summarizer import Summarizer
 import numpy as np
 
 
 st.image("https://previews.123rf.com/images/seamartini/seamartini1708/seamartini170800284/83982313-work-tools-poster-for-hardware-store-design.jpg", caption='Finding items in your favorite retailer', use_column_width=3, clamp=False, channels="RGB")
 
-st.text("What are you looking for?")
+st.subheader("What are you looking for?")
+st.markdown(" ")
+st.subheader("Your item is located in aisle:")
+st.markdown(" ")
+
 
 st.title("Final Project Presentation")
 st.markdown("Joseph Boykin")
@@ -32,6 +32,6 @@ st.markdown(" ")
 st.title("Final Project Reflection")
 st.markdown(" ")
 st.markdown("● Appreciation to Santoshi for assisting me with my final project!")
-st.markdown("● Need to better advocate for myself. ")
+st.markdown("● Need to better advocate for myself, by speaking up sooner if I do not understand the task at hand. ")
 
 
